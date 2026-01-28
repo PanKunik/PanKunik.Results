0cd0a0a6ddeb1b92d91279cc92f910b527b41557
PanKunik <patryk.kunicki@pm.me>
2026-01-28 19:24:21 +0100

    Dev #3 - Refactoring `Error` record

--- src/PanKunik.Results/Error.cs
+++ src/PanKunik.Results/Error.cs
@@ -2,56 +2,14 @@ using PanKunik.Results.Enums;
 
 namespace PanKunik.Results;
 
-public sealed record Error
+public sealed record Error(string Code, string Message, ErrorType Type)
 {
-    public string Code { get; }
-    public string Message { get; }
-    public ErrorType Type { get; }
+    public static Error Failure(string code, string message)
+        => new(code, message, ErrorType.Failure);
 
-    private Error(
-        string code,
-        string message,
-        ErrorType type
-    )
-    {
-        Code = code;
-        Message = message;
-        Type = type;
-    }
+    public static Error Validation(string code, string message)
+        => new(code, message, ErrorType.Validation);
 
-    public static Error Failure(
-        string code,
-        string message
-    )
-    {
-        return new Error(
-            code,
-            message,
-            ErrorType.Failure
-        );
-    }
-
-    public static Error Validation(
-        string code,
-        string message
-    )
-    {
-        return new Error(
-            code,
-            message,
-            ErrorType.Validation
-        );
-    }
-
-    public static Error NotFound(
-        string code,
-        string message
-    )
-    {
-        return new Error(
-            code,
-            message,
-            ErrorType.NotFound
-        );
-    }
+    public static Error NotFound(string code, string message)
+        => new(code, message, ErrorType.NotFound);
 }
